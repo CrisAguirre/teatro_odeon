2452091697f937b1e0a4bb650783cb68410f79e5
Cris <krontroth@gmail.com>
2024-03-24 22:58:24 -0500

    bjh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { PdfViewerModule } from 'ng2-pdf-viewer';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
     AppComponent
